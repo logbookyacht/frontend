49b47703dfd219880af292f64d18ed58e64f849a
ReneRoets <rene.roets@outlook.com>
2019-04-07 18:39:00 +0200

    Added log page
    - Bootstrap package

--- logbook/src/app/app.module.ts
+++ logbook/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { LogComponent } from './log/log.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LogComponent
   ],
   imports: [
     BrowserModule,
