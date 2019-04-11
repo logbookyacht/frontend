c98f64f489c129fb547a9e5bddb166ed3765c996
ReneRoets <rene.roets@outlook.com>
2019-04-11 20:42:36 +0200

    Login UI

--- logbook/src/app/app-routing.module.ts
+++ logbook/src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LogComponent } from './log/log.component';
 import { TripsComponent } from './trips/trips.component';
+import { LoginComponent } from './login/login.component';
+import  {RegisterComponent} from './register/register.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'log', component: LogComponent},
   {path: 'trips', component: TripsComponent},
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
   {path: '**', redirectTo: '/home', pathMatch: 'full'}
 ];
 
