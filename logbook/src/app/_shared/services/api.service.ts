f3e4467934f48c0a91d5a749184706202440383d
ReneRoets <rene.roets@outlook.com>
2019-05-13 22:23:04 +0200

    Rest call

--- logbook/src/app/_shared/services/api.service.ts
+++ logbook/src/app/_shared/services/api.service.ts
@@ -3,30 +3,26 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { map, catchError, tap } from 'rxjs/operators';
+import { User} from '../models/user';
 
-const endpoint = 'http://localhost:8090/';
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type':  'application/json'
-  })
-};
+// const endpoint = 'http://localhost:8090/';
+// const httpOptions = {
+//   headers: new HttpHeaders({
+//     'Content-Type':  'application/json'
+//   })
+// };
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor(private http: HttpClient) { }
+  apiURI: string = 'http://localhost:8090';
 
-  private extractData(res: Response) {
-    let body = res;
-    return body || { };
+  constructor(private httpClient: HttpClient) { }
+
+  public getUsers(){
+    return this.httpClient.get<User[]>(`${this.apiURI}/users`)
   }
-
-  
-login(): Observable<any> {
-  return this.http.post(endpoint + 'users/login','{"email":"rene.roets@outlook.com", "password":"Kaas123"}',httpOptions).pipe(
-    map(this.extractData));
-}
   
 }
--- logbook/src/app/app.component.ts
+++ logbook/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ApiService } from './_shared/services/api.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { User } from '../app/_shared/models/user';
 
 @Component({
   selector: 'app-root',
@@ -12,9 +14,20 @@ export class AppComponent {
 
   constructor(public apiService:ApiService, private route: ActivatedRoute, private router: Router) { }
 
-  Login(){
-      this.apiService.login().subscribe((data)=>{
-        console.log(data);
-        });      
+  getUser(){
+    console.log("Kaas")
+    this.apiService.getUsers().subscribe((res:any)=>{
+      console.log(typeof (res.content as User[]));
+    });
   }
+
+  ngOnInit(){
+    console.log("naampie")
+    this.apiService.getUsers().subscribe((res:any)=>{
+      console.log(typeof (res.content as User[]));
+      
+    });
+
+  }
+
 }
