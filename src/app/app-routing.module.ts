2a368a3200829727d7361ecdd755569502ad4775
ReneRoets <rene.roets@outlook.com>
2019-05-20 20:20:03 +0200

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent } from './components/login/login.component'
 import { HomeComponent } from './components/home/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LogComponent } from './components/log/log.component';
+import { TripsComponent } from './components/trips/trips.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'log', component: LogComponent },
+  { path: 'trips', component: TripsComponent },
 ];
 
 @NgModule({
